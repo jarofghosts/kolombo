44cade3798f1c98fa2cd04cbf38ae20eff20af43
jesse keane <decapitron@gmail.com>
2020-03-29 21:09:06 -0700

    test dark-mode check

--- web/worker.js
+++ web/worker.js
@@ -1,8 +1,10 @@
 self.addEventListener("push", ev => {
+  const useLightIcon = self.matchMedia("(prefers-color-scheme: dark)").matches
   let title = "kolombo"
   let body = "message from kolombo"
-  let icon =
-    "https://raw.githubusercontent.com/jarofghosts/kolombo/master/web/noun-dove.png"
+  let icon = `https://raw.githubusercontent.com/jarofghosts/kolombo/master/web/noun-dove${
+    useLightIcon ? "-inverse" : ""
+  }.png`
   if (ev.data) {
     const data = ev.data.json()
     if (data.title) title = data.title
